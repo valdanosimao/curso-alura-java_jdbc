cd395dc5406f302653e28ac06a15020db9318fd0
valdanosimao <valdanosimao@gmail.com>
2022-04-26 20:44:11 -0300

    criando o pool de conexões

--- src/com/alura/repository/ConnectionFactory.java
+++ src/com/alura/repository/ConnectionFactory.java
@@ -1,16 +1,28 @@
 package com.alura.repository;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 //nome padrão para uma classe de conexão 'ConnectionFactory' Fornecer uma maneira mais simples para criar um objeto
 public class ConnectionFactory {
 
-	public Connection recuperarConexao() throws SQLException {
-		return DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC",
-				"valdano", "3213");
+	public DataSource dataSource;
 
+	public ConnectionFactory() {
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC");
+		comboPooledDataSource.setUser("valdano");
+		comboPooledDataSource.setPassword("3213");
+
+		this.dataSource = comboPooledDataSource;
+	}
+
+	public Connection recuperarConexao() throws SQLException {
+		return this.dataSource.getConnection();
 	}
 
 }
